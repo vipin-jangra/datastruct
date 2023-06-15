64674a47104687866769539a4c77106219de207f
vipin-jangra <vipinsandwal10244@gmail.com>
2023-06-15 22:17:38 +0530

    character array

---
+++ largestWord.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    cin.ignore();
+
+    char arr[n+1];
+    cin.getline(arr,n);
+    cin.ignore();
+
+    int i=0;
+    int currlen=0, maxlen =0;
+
+    while(1){
+
+        if(arr[i] == ' ' || arr[i] == '\0'){
+            if(currlen > maxlen)
+                maxlen = currlen;
+
+            currlen = 0;
+        }
+        else currlen++;
+
+        if(arr[i] == '\0'){
+            break;
+        }
+        i++;
+    }
+
+    cout<<maxlen<<endl;
+}
